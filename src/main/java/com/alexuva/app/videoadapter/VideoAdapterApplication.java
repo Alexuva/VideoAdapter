6e8d59218010ceebae7a0435482c92513b7cf667
Alejandro Hernández Rosa <alejandrohernandezrosa@gmail.com>
2026-03-15 19:07:04 +0100

    Change view name from hello-view to home

--- src/main/java/com/alexuva/app/videoadapter/VideoAdapterApplication.java
+++ src/main/java/com/alexuva/app/videoadapter/VideoAdapterApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class VideoAdapterApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(VideoAdapterApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(VideoAdapterApplication.class.getResource("home.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 600);
         VideoAdapterController controller = fxmlLoader.getController();
         stage.setOnCloseRequest(event -> controller.cancel());
